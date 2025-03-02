ef3e97bb898ea79142bb317e8bc1e7e49c93a207
Dubul13454 <rakov2052@gmail.com>
2025-03-02 22:12:03 +0300

    Update поворот.js

--- поворот тест/поворот.js
+++ поворот тест/поворот.js
@@ -2,5 +2,5 @@ const rotateButton = document.getElementById('rotateButton');
 const box = document.getElementById('box');
 
 rotateButton.addEventListener('click', () => {
-    box.classList.toggle('rotated'); // Переключаем класс для поворота
+    img.classList.toggle('rotated'); // Переключаем класс для поворота
 });
