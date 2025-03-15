3d0ad3c42a480a78492cc770322d7c86d87e0c16
Dubul13454 <rakov2052@gmail.com>
2025-03-15 16:15:26 +0300

    Delete поворот.js

--- поворт тест/поворот.js
+++
@@ -1,6 +0,0 @@
-const rotateButton = document.getElementById('rotateButton');
-const box = document.getElementById('box');
-
-rotateButton.addEventListener('click', () => {
-    box.classList.toggle('rotated'); // Переключаем класс для поворота
-});
